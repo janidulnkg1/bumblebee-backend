91ab27a6d20560e0cf4eb8917b4b64f3780eece6
Janidu Lankage <janidulnkg1@gmail.com>
2023-04-02 02:55:30 +0530

    commit get-users

--- src/main/java/com/example/bumblebeebackend/controller/UserController.java
+++ src/main/java/com/example/bumblebeebackend/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
 @RestController
@@ -31,6 +32,11 @@ public class UserController {
         } else {
             return ResponseEntity.ok("Login successful");
         }
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 
 }
