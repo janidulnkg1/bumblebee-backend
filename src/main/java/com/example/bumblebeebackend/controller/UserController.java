92f5f421053124a5d0bc7295aa1321b084d79653
Janidu Lankage <janidulnkg1@gmail.com>
2023-04-02 02:25:39 +0530

    commit login page request

--- src/main/java/com/example/bumblebeebackend/controller/UserController.java
+++ src/main/java/com/example/bumblebeebackend/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.bumblebeebackend.repository.UserRepository;
 import com.example.bumblebeebackend.model.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +23,14 @@ public class UserController {
         return ResponseEntity.ok("User created successfully");
     }
 
-
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        User existingUser = userRepository.findByemail(user.getEmail());
+        if (existingUser == null || !existingUser.getPassword().equals(user.getPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
+        } else {
+            return ResponseEntity.ok("Login successful");
+        }
+    }
 
 }
--- src/main/java/com/example/bumblebeebackend/repository/UserRepository.java
+++ src/main/java/com/example/bumblebeebackend/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.example.bumblebeebackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long>{
+    User findByemail(String email);
 }
